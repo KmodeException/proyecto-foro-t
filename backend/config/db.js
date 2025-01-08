d95c11d1aedf8a6a07084dec32f0c3ec44472d14
luis mansilla <117701939+KmodeException@users.noreply.github.com>
2025-01-08 05:05:20 -0300

    refactor(db): update database configuration to use ES modules

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,19 +1,18 @@
 //backend/config/db.js
 
-
-const mongoose = require('mongoose');
+import mongoose from 'mongoose';
 
 const connectDB = async () => {
     try {
-        const conn = await mongoose.connect(process.env.MONGODB_URI, {
+        const conn = await mongoose.connect(process.env.MONGO_URI, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
         });
-        console.log(`MongoDB Connected: ${conn.connection.host}`);
+        console.log(`✅ MongoDB Connected: ${conn.connection.host}`);
     } catch (error) {
-        console.error(`Error: ${error.message}`);
+        console.error(`❌ Error: ${error.message}`);
         process.exit(1);
     }
 };
 
-module.exports = connectDB;
+export default connectDB;

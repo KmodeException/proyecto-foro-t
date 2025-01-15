59f6ef12f75af02260d82813b6cc95ae646b2e5d
luis mansilla <117701939+KmodeException@users.noreply.github.com>
2025-01-15 06:02:32 -0300

    feat(karma): crear middleware de verificación de karma

---
+++ backend/middleware/karmaCheck.js
@@ -0,0 +1,36 @@
+import { ReputationService } from '../modules/reputation/reputation.service.js';
+
+export const karmaCheck = (action) => {
+    return async (req, res, next) => {
+        try {
+            const restrictions = ReputationService.checkRestrictions(req.user.reputation);
+            
+            switch(action) {
+                case 'createThread':
+                    if (!restrictions.canCreateThreads) {
+                        return res.status(403).json({ 
+                            message: 'Karma insuficiente para crear hilos' 
+                        });
+                    }
+                    break;
+                case 'comment':
+                    if (!restrictions.canComment) {
+                        return res.status(403).json({ 
+                            message: 'Karma insuficiente para comentar' 
+                        });
+                    }
+                    break;
+                case 'vote':
+                    if (restrictions.readOnly) {
+                        return res.status(403).json({ 
+                            message: 'No puedes votar con karma negativo' 
+                        });
+                    }
+                    break;
+            }
+            next();
+        } catch (error) {
+            res.status(500).json({ message: error.message });
+        }
+    };
+};

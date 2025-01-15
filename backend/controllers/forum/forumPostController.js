5fad0e8374d1e855082b4b3c55371b878b3052fd
luis mansilla <117701939+KmodeException@users.noreply.github.com>
2025-01-15 05:47:57 -0300

    feat(forum): integrar sistema de reputación con votos

--- backend/controllers/forum/forumPostController.js
+++ backend/controllers/forum/forumPostController.js
@@ -5,6 +5,11 @@ import { REPUTATION_ACTIONS } from '../../modules/reputation/reputation.constant
 export const forumPostController = {
     create: async (req, res) => {
         try {
+            const restrictions = ReputationService.checkRestrictions(req.user.reputation);
+            if (restrictions.readOnly) {
+                return res.status(403).json({ message: 'Karma insuficiente para crear posts' });
+            }
+
             const { title, content, threadId } = req.body;
             const post = new ForumPost({
                 title,
@@ -13,7 +18,7 @@ export const forumPostController = {
                 author: req.user._id
             });
             await post.save();
-            await post.populate('author', 'username');
+            await post.populate('author', 'username reputation level');
             res.status(201).json(post);
         } catch (error) {
             res.status(500).json({ message: error.message });
@@ -40,37 +45,22 @@ export const forumPostController = {
                 return res.status(404).json({ message: 'Post no encontrado' });
             }
 
-            const voteField = type === 'up' ? 'votes.up' : 'votes.down';
-            const oppositeField = type === 'up' ? 'votes.down' : 'votes.up';
-
-            // Remover voto opuesto si existe
+            // Actualizar votos
+            const voteField = `votes.${type}`;
             await ForumPost.updateOne(
                 { _id: post._id },
-                { $pull: { [oppositeField]: req.user._id } }
+                { $addToSet: { [voteField]: req.user._id } }
             );
 
-            // Alternar voto actual
-            if (post[type === 'up' ? 'votes.up' : 'votes.down'].includes(req.user._id)) {
-                await ForumPost.updateOne(
-                    { _id: post._id },
-                    { $pull: { [voteField]: req.user._id } }
-                );
-            } else {
-                await ForumPost.updateOne(
-                    { _id: post._id },
-                    { $addToSet: { [voteField]: req.user._id } }
-                );
-
-                // Actualizar reputación
-                await ReputationService.updateReputation(
-                    post.author,
-                    type === 'up' ? REPUTATION_ACTIONS.COMMUNITY.UPVOTE : REPUTATION_ACTIONS.COMMUNITY.DOWNVOTE,
-                    post._id
-                );
-            }
+            // Actualizar reputación
+            await ReputationService.updateReputation(
+                post.author,
+                type === 'up' ? REPUTATION_ACTIONS.COMMUNITY.POST_UPVOTE : REPUTATION_ACTIONS.COMMUNITY.POST_DOWNVOTE,
+                post._id
+            );
 
             const updatedPost = await ForumPost.findById(post._id)
-                .populate('author', 'username');
+                .populate('author', 'username reputation level');
             
             res.json(updatedPost);
         } catch (error) {

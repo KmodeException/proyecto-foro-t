50455991348a16ca2601ae413ddf086e640bc246
luis mansilla <117701939+KmodeException@users.noreply.github.com>
2025-01-16 01:03:23 -0300

    docs: añadir documentación OpenAPI completa para tipos de reputación

--- backend/modules/reputation/reputation.types.js
+++ backend/modules/reputation/reputation.types.js
@@ -5,23 +5,44 @@
  *     ReputationType:
  *       type: string
  *       enum: [translation, community, moderation]
+ *       description: Tipos de acciones que afectan la reputación
+ *     
  *     ReputationLevel:
  *       type: object
+ *       required:
+ *         - name
+ *         - minPoints
  *       properties:
  *         name:
  *           type: string
  *           enum: [Novato, Contribuidor, Experto]
+ *           description: Nivel del usuario basado en puntos
  *         minPoints:
  *           type: number
+ *           description: Puntos mínimos requeridos para el nivel
  *         permissions:
  *           type: object
  *           properties:
  *             canCreateThreads:
  *               type: boolean
+ *               description: Puede crear hilos en el foro
  *             canComment:
  *               type: boolean
+ *               description: Puede comentar en posts
  *             canVote:
  *               type: boolean
+ *               description: Puede votar en el foro
+ *     
+ *     ReputationHistory:
+ *       type: object
+ *       properties:
+ *         action:
+ *           $ref: '#/components/schemas/ReputationType'
+ *         points:
+ *           type: number
+ *         timestamp:
+ *           type: string
+ *           format: date-time
  */
 
 export const ReputationTypes = {
